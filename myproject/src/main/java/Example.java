145a6825f6677817f24de72db76b091569927586
sumeragizzz <developer@sumeragizzz.dev>
2020-06-15 21:42:53 +0900

    change package

--- myproject/src/main/java/Example.java
+++ myproject/src/main/java/Example.java
@@ -1,5 +1,3 @@
-package dev.sumeragizzz.springpractice.myproject;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
